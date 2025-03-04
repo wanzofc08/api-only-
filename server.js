bff675eac5b31645810bc56bf9fd5613ecf425bd
wanzofc08 <wanzofc08@gmail.com>
2025-03-05 06:29:37 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 8080;
+
+app.use(cors());
+app.use(express.json({limit: '5mb'}));
+
+let messages = [];
+
+app.get('/messages', (req, res) => {
+    res.json(messages);
+});
+
+app.post('/messages', (req, res) => {
+    const newMessage = req.body;
+    messages.push(newMessage);
+
+    console.log('Pesan baru diterima:', newMessage);
+    res.status(201).send('Pesan berhasil dikirim!');
+});
+
+app.listen(port, () => {
+    console.log(`Server backend berjalan di port ${port}`);
+});
