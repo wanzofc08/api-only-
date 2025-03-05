6ad18d371652f3108f041a07119999f3a0b2fcf8
wanzofc08 <wanzofc08@gmail.com>
2025-03-05 07:00:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const port = 8080;
 
 app.use(cors());
-app.use(express.json({limit: '5mb'}));
+app.use(express.json({
+    limit: '300mb'
+}));
 
 let messages = [];
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
 
 app.get('/messages', (req, res) => {
     res.json(messages);
@@ -19,9 +18,22 @@ app.get('/messages', (req, res) => {
 app.post('/messages', (req, res) => {
     const newMessage = req.body;
     messages.push(newMessage);
-
     console.log('Pesan baru diterima:', newMessage);
     res.status(201).send('Pesan berhasil dikirim!');
+});
+
+app.post('/admin/clear', (req, res) => {
+    messages = [];
+    console.log('Chat cleared by admin');
+    res.status(200).send('Chat cleared successfully.');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'admin.html'));
 });
 
 app.listen(port, () => {
